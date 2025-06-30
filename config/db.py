a48097a349c18022f580dcff20618eb4db66f37b
Maryann Mwikali <mwikali119@gmail.com>
2025-06-30 12:34:38 +0300

    Add a base for all models created

--- config/db.py
+++ config/db.py
@@ -1,6 +1,10 @@
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 import os
 
 database_url = os.getenv("DATABASE_URL")
 engine = create_engine(database_url)
 conn = engine.connect()
+
+Base = declarative_base()
+
