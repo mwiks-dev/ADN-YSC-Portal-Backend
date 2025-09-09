5fd157708ad9fd2c9aff429b58010fa493af3b0d
ngugikiarie_ <ngugithe4th@gmail.com>
2025-09-09 11:20:47 +0300

    Return profile pic on login

--- routers/graphql/user_schema.py
+++ routers/graphql/user_schema.py
@@ -12,11 +12,11 @@ from passlib.context import CryptContext
 from utils.auth_utils import get_current_user, can_register_users
 from models.user import User
 import imghdr
-import pprint
+import os
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
+BASE_URL = os.getenv("BASE_URL", "http://localhost:8000")
     
 @strawberry.type
 class UserQuery:
@@ -58,6 +58,7 @@ class UserQuery:
                     dateofbirth = user.dateofbirth,
                     idnumber = user.idnumber,
                     baptismref = user.baptismref,
+                    profile_pic= user.profile_pic,
                     role=RoleEnum(user.role.value), 
                     status = UserStatus(user.status.value), # Convert from SQLAlchemy Enum to Strawberry Enum
                     parish=user.parish
@@ -114,11 +115,15 @@ class UserMutation:
         token = create_access_token(data={"sub": user.email})
         return LoginPayload(
             token = TokenType(access_token=token, token_type="bearer"),
-            user = UserType(id=user.id, name=user.name, email=user.email, phonenumber=user.phonenumber,dateofbirth=user.dateofbirth, idnumber=user.idnumber, baptismref= user.baptismref, role=user.role,status=user.status, parish=user.parish)
+            user = UserType(id=user.id, name=user.name, email=user.email, phonenumber=user.phonenumber,dateofbirth=user.dateofbirth, idnumber=user.idnumber, baptismref= user.baptismref, role=user.role,status=user.status, parish=user.parish, profile_pic=user.profile_pic)
         )
     @strawberry.mutation 
     async def upload_profile_pic(self,user_id:int, file:Upload) -> UploadProfilePicResponse:
-    
+        db = SessionLocal()
+        user = db.query(User).get(user_id)
+        if not user:
+            raise Exception("User not found.")
+        
         allowed_types = ['jpeg','png','jpg']
         file_type = imghdr.what(file.file)
         if file_type not in allowed_types:
@@ -133,15 +138,10 @@ class UserMutation:
         contents = await file.read()
         filename = f"user_{user_id}_profile.{file_type}"
         filepath = f"static/profile_pics/{filename}"
-
         with open(filepath, "wb") as f:
-            f.write(file.file.read())
-
+            f.write(contents) 
         # Update the user in DB
-        db = SessionLocal()
-        user = db.query(User).get(user_id)
-        if not user:
-            raise Exception("User not found.")
+      
         user.profile_pic = filename
         db.commit()
         db.refresh(user)
