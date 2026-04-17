01a5c4ce168b0cf13f93dfd2c5c812a21fbeff4b
ngugikiarie_ <ngugithe4th@gmail.com>
2026-04-17 15:49:39 +0300

    Update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,16 +2,16 @@ module.exports = {
     apps: [
         {
             name: "fastapi-app",
-            script: "/home/adnyouth/virtualenv/python/main/3.12/bin/uvicorn",
-            args: "main:app --host 127.0.0.1 --port 5000",
+            script: "/home1/adnyouth/virtualenv/backend/3.12/bin/uvicorn",
+            args: "main:app --host 127.0.0.1 --port 5000 --reload",
             interpreter: "none",
-            cwd: "/home1/adnyouth/python/main",
+            cwd: "/home1/adnyouth/backend",
             watch: false,
             autorestart: true,
             env: {
-                PATH: "/home/adnyouth/virtualenv/python/main/3.12/bin:/usr/local/bin:/usr/bin:/bin",
-                PYTHONPATH: "/home1/adnyouth/python/main",
-                VIRTUAL_ENV: "/home/adnyouth/virtualenv/python/main/3.12",
+                PATH: "/home1/adnyouth/virtualenv/backend/3.12/bin:/usr/local/bin:/usr/bin:/bin",
+                PYTHONPATH: "/home1/adnyouth/backend",
+                VIRTUAL_ENV: "/home1/adnyouth/virtualenv/backend/3.12",
             },
         },
     ],
